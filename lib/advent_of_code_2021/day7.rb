d8843ced60dabc41f85189b700a1a1eb629aa427
Rachel Evans <rachel@rachelevans.org>
2021-12-07 07:05:40 +0100

    Day 7. Brute force :-(

---
+++ lib/advent_of_code_2021/day7.rb
@@ -0,0 +1,27 @@
+module AdventOfCode2021
+  class Day7
+    class Part1 < Base
+      def calculate(lines)
+        positions = lines.first.chomp.split(',').map(&:to_i)
+
+        brute_force = (positions.min..positions.max).map do |pos|
+          [pos, positions.map { |x| (x - pos).abs }.sum]
+        end.sort_by(&:last)
+
+        brute_force.first.last
+      end
+    end
+
+    class Part2 < Part1
+      def calculate(lines)
+        positions = lines.first.chomp.split(',').map(&:to_i)
+
+        brute_force = (positions.min..positions.max).map do |pos|
+          [pos, positions.map { |x| n = (x - pos).abs; n * (n+1) / 2 }.sum]
+        end.sort_by(&:last)
+
+        brute_force.first.last
+      end
+    end
+  end
+end
---
+++ spec/day7_spec.rb
@@ -0,0 +1,21 @@
+require_relative './spec_helper'
+
+describe AdventOfCode2021::Day7 do
+  let (:example) {
+    <<~DATA
+      16,1,2,0,4,2,7,1,2,14
+    DATA
+  }
+
+  describe AdventOfCode2021::Day7::Part1 do
+    it "handles the test data" do
+      test(example, 37)
+    end
+  end
+
+  describe AdventOfCode2021::Day7::Part2 do
+    it "handles the test data" do
+      test(example, 168)
+    end
+  end
+end
