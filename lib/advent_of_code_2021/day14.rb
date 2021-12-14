45487e44ccde86865a432477dcf49f96bf85825a
Rachel Evans <rachel@rachelevans.org>
2021-12-14 15:01:48 +0000

    Refactor

--- lib/advent_of_code_2021/day14.rb
+++ lib/advent_of_code_2021/day14.rb
@@ -6,11 +6,24 @@ module AdventOfCode2021
       def calculate(lines)
         template, rules = parse(lines)
 
-        10.times do
-          template = mutate(template, rules)
+        pair_counts = Hash.new { |h, k| h[k] = 0 }
+
+        template.chars.each_cons(2) do |pair|
+          pair_counts[pair] += 1
         end
 
-        score(template.chars)
+        iterations.times do
+          pair_counts = mutate(pair_counts, rules)
+        end
+
+        letter_counts = count_letters(pair_counts, template)
+
+        min, max = letter_counts.values.minmax
+        max - min
+      end
+
+      def iterations
+        10
       end
 
       def parse(lines)
@@ -23,75 +36,6 @@ module AdventOfCode2021
         end.to_h
 
         [template, rules]
-      end
-
-      def mutate(template, rules)
-        output = "".dup
-
-        template.chars.each do |char|
-          if output == ""
-            output << char
-          else
-            key = output[-1] + char
-            extra = rules[key]
-            raise "No extra for #{key}" unless extra
-
-            output << extra << char
-          end
-        end
-
-        output
-      end
-
-      def score(chars)
-        scores = chars.tally.entries.sort_by(&:last)
-        scores.last[1] - scores.first[1]
-      end
-    end
-
-    class Part2Slow < Part1
-      # Probably works. Still doesn't scale.
-      def calculate(lines)
-        template, rules = parse(lines)
-        sequence = template.each_char.lazy
-
-        40.times do
-          sequence = wrap(sequence, rules)
-        end
-
-        score(sequence)
-      end
-
-      def wrap(input, rules)
-        return enum_for(:wrap, input, rules) unless block_given?
-
-        previous = nil
-        input.each do |char|
-          yield rules[previous + char] if previous
-          yield char
-          previous = char
-        end
-      end
-    end
-
-    class Part2 < Part1
-      def calculate(lines)
-        template, rules = parse(lines)
-
-        pair_counts = Hash.new { |h, k| h[k] = 0 }
-
-        template.chars.each_cons(2) do |pair|
-          pair_counts[pair] += 1
-        end
-
-        40.times do
-          pair_counts = mutate(pair_counts, rules)
-        end
-
-        letter_counts = count_letters(pair_counts, template)
-
-        min, max = letter_counts.values.minmax
-        max - min
       end
 
       def mutate(pair_counts, rules)
@@ -118,6 +62,12 @@ module AdventOfCode2021
         counts[template.chars.last] += 1
 
         counts.transform_values { |n| n / 2 }
+      end
+    end
+
+    class Part2 < Part1
+      def iterations
+        40
       end
     end
   end
